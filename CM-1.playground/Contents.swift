7a4c29ee25c5d9b972b42d601d4c60d1b0337ab9
Brenava <50605870+Brenava@users.noreply.github.com>
2019-12-09 13:00:03 -0600

    Brenda

---
+++ CM-1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+var nomVariable = "Hola" //Variable que cambia en lo largo del programa
+let nomConstante = 20 //Constante
+type (of: nomVariable)
+/*
+ string
+ Int
+ Double
+ Float
+ Bool
+ */
+
+print(nomConstante)
+
+/* Definicion de tipos de datos propios
+ struct, class
+ 
+ */
